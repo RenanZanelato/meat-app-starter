bf4b0cbd15d11a4e18b3b39fc13574988b9218c3
Renan Zanelato <renanzanelato96@gmail.com>
2020-01-22 15:16:35 -0300

    Shame

--- src/app/restaurant-order/restaurant-order.component.ts
+++ src/app/restaurant-order/restaurant-order.component.ts
@@ -4,12 +4,17 @@ import { OrderService } from './restaurant-order.service';
 import { CartItem } from 'app/restaurant-detail/restaurant-cart/restaurant-cart-item.model';
 import { Order, OrderItem } from './restaurant-order.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-restaurant-order',
   templateUrl: './restaurant-order.component.html'
 })
 export class RestaurantOrderComponent implements OnInit {
+
+  orderForm: FormGroup;
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern= /^[0-9]*$/;
 
   paymentOptions: RadioOption[] =[
     {label: 'Dinheiro',value: 1},
@@ -20,10 +25,33 @@ export class RestaurantOrderComponent implements OnInit {
 
   delivery: number = 8;
 
-  constructor(private orderService: OrderService, private router: Router ) { }
+  constructor(private orderService: OrderService, private router: Router,private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
+    
+    this.orderForm = this.formBuilder.group({
+      name:this.formBuilder.control('',[Validators.required,Validators.minLength(5)]),
+      email: this.formBuilder.control('',[Validators.required,Validators.minLength(5),Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('',[Validators.required,Validators.minLength(5),Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('',[Validators.required,Validators.minLength(5),Validators.pattern(this.numberPattern)]),
+      number:this.formBuilder.control('',[Validators.required,Validators.minLength(5)]),
+      complement:this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('',[Validators.required])
+    },{validator: RestaurantOrderComponent.equalsTo})
   }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean}{
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if(!email || !emailConfirmation){
+      return undefined;
+    }
+    if(email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true};
+    }
+    return undefined;
+  }
+
   itemsValue(): number{
     return this.orderService.itemsValue();
   }
